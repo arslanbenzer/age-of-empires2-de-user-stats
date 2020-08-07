ea7e720268197d90f263f838aa35945da6f21ced
arslanbenzer <muhammedarslanbenzer@gmail.com>
2020-08-07 14:34:41 +0300

    line chart for rating history

--- src/table_ratings.js
+++ src/table_ratings.js
@@ -8,6 +8,9 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
+import '../node_modules/react-vis/dist/style.css';
+
+import { XYPlot, LineSeries, VerticalGridLines, HorizontalGridLines, XAxis, YAxis } from 'react-vis';
 
 const useStyles = makeStyles({
   table: {
@@ -19,31 +22,46 @@ const TableRatings = ({ matches }) => {
 
   const classes = useStyles();
 
+  var data = [];
+
+  for (var i = matches.length - 1; i >= 0; i -= 1) {
+    data.push({ "x": new Date(matches[i].timestamp * 1000).getTime(), "y": matches[i].rating })
+  }
+
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>#</TableCell>
-            <TableCell>Rating</TableCell>
-            <TableCell>Num Wins</TableCell>
-            <TableCell>Date</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {(matches.length > 0) ? matches.map((droplet, index) => {
-            return (
-              <TableRow key={index}>
-                <TableCell >{index + 1}</TableCell>
-                <TableCell >{droplet.rating}</TableCell>
-                <TableCell >{droplet.num_wins}</TableCell>
-                <TableCell >{new Date(droplet.timestamp * 1000).toLocaleString()}</TableCell>
-              </TableRow >
-            )
-          }) : <tr><td colSpan="5">Loading...</td></tr>}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div>
+      <XYPlot xType="time" height={300} width={1600}>
+        <HorizontalGridLines />
+        <VerticalGridLines />
+        <XAxis title="Time" />
+        <YAxis title="Rating" />
+        <LineSeries data={data} lineStyle={{ stroke: "red" }} markStyle={{ stroke: "blue" }} />
+      </XYPlot>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>#</TableCell>
+              <TableCell>Rating</TableCell>
+              <TableCell>Num Wins</TableCell>
+              <TableCell>Date</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {(matches.length > 0) ? matches.map((droplet, index) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell >{index + 1}</TableCell>
+                  <TableCell >{droplet.rating}</TableCell>
+                  <TableCell >{droplet.num_wins}</TableCell>
+                  <TableCell >{new Date(droplet.timestamp * 1000).toLocaleString()}</TableCell>
+                </TableRow >
+              )
+            }) : <tr><td colSpan="5">Loading...</td></tr>}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 }
 
